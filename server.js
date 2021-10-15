37022896767dc847ceaf424a052c261c08ab40c6
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2021-10-16 01:26:58 +0200

    fix

--- server.js
+++ server.js
@@ -1,44 +1,68 @@
 const express = require('express')
-const { spawn } = require('child_process')
+const { exec } = require('child_process')
+const fs = require('fs')
 
 const app = express()
 
 app.use(express.json())
 
 app.post('/', (req, res, next) => {
-  let commands = req.body.script
-    .replace(/\n+/g, '\n')
-    .split('\n')
-    .join(' ~ ')
+  fs.writeFile('tmp.py', req.body.script, (err) => {
+    if (err) {
+      res.send({
+        error: err
+      })
+      return
+    }
 
-  let stdout = ''
-  let stderr = ''
+    exec('python3 pyboard.py tmp.py', (err) => {
+    if (err) {
+        res.send({
+          error: err
+        })
+        return
+      }
 
-  commands = `~ ${commands} ~`
-
-  const cmd = spawn('rshell', [
-    'repl',
-    commands
-  ])
-
-  cmd.stdout.on('data', (data) => {
-    stdout += `${data}`
-    console.log(`stdout: ${data}`);
-  });
-  
-  cmd.stderr.on('data', (data) => {
-    stderr += `${data}`
-    console.error(`stderr: ${data}`);
-  });
-
-  cmd.on('close', (code) => {
-    console.log('Hi')
-    res.send({
-      stdout,
-      stderr,
-      ok: true
+      res.send({
+        ok: true
+      })
     })
   })
+
+
+  // let commands = req.body.script
+  //   .replace(/\n+/g, '\n')
+  //   .split('\n')
+  //   .join(' ~ ')
+
+  // let stdout = ''
+  // let stderr = ''
+
+  // commands = `~ ${commands} ~`
+
+  // const cmd = spawn('rshell', [
+  //   'repl',
+  //   commands
+  // ])
+
+  // cmd.stdout.on('data', (data) => {
+  //   stdout += `${data}`
+  //   console.log(`stdout: ${data}`);
+  // });
+  
+  // cmd.stderr.on('data', (data) => {
+  //   stderr += `${data}`
+  //   console.error(`stderr: ${data}`);
+  // });
+
+  // cmd.on('close', (code) => {
+  //   console.log('Hi')
+  //   res.send({
+  //     stdout,
+  //     stderr,
+  //     ok: true
+  //   })
+  // })
 })
 
 app.listen(4000, () => {
